c2797ff7e3860823501bd60a1bbf886d5dc8f7ce
Hogne <hsu014@post.uit.no>
2025-02-07 10:33:15 +0100

    B-spline least square

--- qmldemo-master/application/my_bspline.h
+++ qmldemo-master/application/my_bspline.h
@@ -20,24 +20,23 @@ public:
     MyBSpline( const MyBSpline<T>& copy );
     virtual ~MyBSpline();
 
-    bool                isClosed() const override;
+    bool                    isClosed() const override;
 
 protected:
     // Virtual functions from PCurve, which have to be implemented locally
-    void                eval( T t, int d, bool /*l*/ ) const override;
-    T                   getStartP() const override;
-    T                   getEndP()   const override;
-
-    // Protected data for the curve
+    void                    eval( T t, int d, bool /*l*/ ) const override;
+    T                       getStartP() const override;
+    T                       getEndP()   const override;
 
 private:
-    std::vector<T>          _t;                     // knot vector
-    DVector<Vector<T,3>>    _c;                     // control points
+    std::vector<T>          _t;                                 // knot vector
+    DVector<Vector<T,3>>    _c;                                 // control points
+    int                     _n;                                 // Number of control points
 
-    T           W(int d, int i, T t) const;         // Map domain to 0-1
-    Vector<T,3> B(int i, T t) const;                // Basis functions for given t
-    int         find_i(T t) const;                  // Find index i for given t
-    void        make_knot_vector(int n, int d=2);   // Create the knot vector _t
+    T                       W(int d, int i, T t) const;         // Map domain to 0-1
+    Vector<T,3>             B(int i, T t) const;                // Basis functions for given t
+    int                     find_i(T t) const;                  // Find index i for given t
+    void                    make_knot_vector(int n, int d, T s, T e);   // Create the knot vector _t
 
 }; // END class MyBSpline
 
@@ -47,9 +46,15 @@ private:
 template <typename T>
 inline
     MyBSpline<T>::MyBSpline(const DVector<Vector<T,3>>& c) : PCurve<T,3>(20, 0, 0), _c(c) {
-    std::cout << "Control points _c: " << _c << std::endl;
 
-    make_knot_vector(c.getDim());
+    // _c = c in start of constructor
+    std::cout << "Control points _c: " << _c << std::endl;
+    _n = c.getDim();
+
+    int d = 2;                                      // Degree of b-spline hardcoded as 2
+    make_knot_vector(_n, d, 0, _n-d);
+
+
 }
 
 
@@ -57,8 +62,42 @@ inline
 // Constructor 2
 template <typename T>
 inline
-    MyBSpline<T>::MyBSpline(const DVector<Vector<T,3>>& p, int n) : PCurve<T,3>(20, 0, 0) {
-    // make the c-vector, then same as above
+    MyBSpline<T>::MyBSpline(const DVector<Vector<T,3>>& p, int n) : PCurve<T,3>(20, 0, 0), _n(n) {
+
+    int m = p.getDim();                             // Number of points
+    std::vector<T> x(m);                            // Total distance traveled in each point
+
+    x[0] = 0;
+    for (int i = 1; i < m; i++) {
+        x[i] = x[i-1] + (p[i] - p[i-1]).getLength();
+    }
+
+    make_knot_vector(_n, 2, x[0], x.back());
+
+    DMatrix<T> A(m, _n, T(0));
+    for (int j=0; j<m; j++) {
+        int i = find_i(x[j]);
+
+        auto b = B(i, x[j]);
+        A[j][i-2] = b[0];
+        A[j][i-1] = b[1];
+        A[j][i] = b[2];
+    }
+
+    DMatrix<T> A_t(A);
+    A_t.transpose();
+
+    DMatrix<T> B = A_t*A;
+
+    DVector<Vector<T,3>> y = A_t*p;
+
+    DMatrix<T> B_inv(B);
+    B_inv.invert();
+
+    DVector<Vector<T,3>> c = B_inv * y;
+    std::cout << "c: " << c << std::endl;
+
+    _c = c;
 }
 
 
@@ -69,6 +108,7 @@ inline
     MyBSpline<T>::MyBSpline( const MyBSpline<T>& copy ) : PCurve<T,3>(copy) {
     _t = copy._t;
     _c = copy._c;
+    _n = copy._n;
 }
 
 
@@ -139,6 +179,21 @@ Vector<T,3> MyBSpline<T>::B(int i, T t) const {
 
     b[2] = (W(1, i, t))*(W(2, i, t));           // b_i;
 
+    // const int d = 2;
+    // Vector<T,(d+1)> b2;
+
+    // b2[0] = 0;
+    // for (j=1; j<=d; j++) {
+    //     b2[j] = W(j, i, t) * b2[j-1];
+
+    //     for (k=j-1; k>0; k--) {
+    //         b2[k] = W(j, i-j+k, t) * b2[k-1] +
+    //                 (1 - W(j, i-j+k+1, t)) * b2[k];
+    //     }
+    //     b2[0] =
+    // }
+
+
     return b;
 }
 
@@ -146,30 +201,28 @@ Vector<T,3> MyBSpline<T>::B(int i, T t) const {
 
 template <typename T>
 int MyBSpline<T>::find_i(T t) const {
-    int i = 2; // = d
-    int n = _c.getDim();
-    for (; i<n; i++) {
+    int i = 2;
+
+    for (; i<_n; i++) {
         if (_t[i+1] > t) {
-            //std::cout << "Index: " << i << std::endl;
             return i;
         }
     }
-    //std::cout << "Index: " << n-1 << std::endl;
-    return n-1;
+
+    return _n-1;
 }
 
 
 
 template <typename T>
-void MyBSpline<T>::make_knot_vector(int n, int d) {
-    for (int i = 0; i<=d; i++) _t.push_back(0);
+void MyBSpline<T>::make_knot_vector(int n, int d, T s, T e) {
+    for (int i = 0; i<=d; i++) _t.push_back(s);
 
-    for (int i = d+1; i<n; i++) _t.push_back(i-d);
+    for (int i = d+1; i<n; i++) _t.push_back((e-s)/(n-d)*(i-d));
 
-    for (int i = 0; i<=d; i++) _t.push_back(n-d);
+    for (int i = 0; i<=d; i++) _t.push_back(e);
 
     std::cout << "Knot vector _t: " << _t << std::endl;
-
 }
 
 
